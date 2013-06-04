7c5711fd82b147ee0c4ecf0be3a8d98a9090a671
Mostafa Sameh <msameh@cloud9ers.com>
2013-06-04 07:52:40 +0200

    mahout first

--- mahout/mahout-first/src/main/scala/dosht/examples/mahout/MahoutFirst.scala
+++ mahout/mahout-first/src/main/scala/dosht/examples/mahout/MahoutFirst.scala
@@ -6,13 +6,42 @@ import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity
 import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood
 import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender
 import scala.collection.JavaConversions._
+import org.apache.mahout.common.RandomUtils
+import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommenderEvaluator
+import org.apache.mahout.cf.taste.eval.RecommenderBuilder
+import org.apache.mahout.cf.taste.model.DataModel
+import org.apache.mahout.cf.taste.recommender.Recommender
 
 object MahoutFirst extends App {
-  val ratingsFilePath = "data/ratings.dat"
-  val model = new FileDataModel(new File(ratingsFilePath))
-  val similarity = 	new PearsonCorrelationSimilarity(model)
-  val neighborhood = new NearestNUserNeighborhood(2, similarity, model)
-  val recomender = new GenericUserBasedRecommender(model, neighborhood, similarity)
-  val recomindations = recomender.recommend(1, 2)
-  recomindations.foreach(println)
+  println("recommender: -----------------------")
+  recommend
+  println("evaluation: ----------------------------")
+  evaluate
+
+  lazy val model = {
+    val ratingsFilePath = "data/ratings.dat"
+    new FileDataModel(new File(ratingsFilePath))
+  }
+
+  def recommender = {
+    val similarity = new PearsonCorrelationSimilarity(model)
+    val neighborhood = new NearestNUserNeighborhood(2, similarity, model)
+    new GenericUserBasedRecommender(model, neighborhood, similarity)
+  }
+
+  def recommend {
+	  recommender.recommend(1, 10).foreach(println)
+  }
+
+  def evaluate {
+    RandomUtils.useTestSeed()
+    val evaluater = new AverageAbsoluteDifferenceRecommenderEvaluator
+    val builder = new RecommenderBuilder {
+      def buildRecommender(dataModel: DataModel): Recommender = {
+        recommender
+      }
+    }
+    val score = evaluater.evaluate(builder, null, model, 0.7, 1.0)
+    println(s"score: $score")
+  }
 }
