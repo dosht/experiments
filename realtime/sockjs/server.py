9f1913a49d44ed6b1923d04a33d719a72e8b8737
Mostafa Sameh <msameh@cloud9ers.com>
2013-07-17 15:27:30 +0200

    sockjs on tornadio example

---
+++ realtime/sockjs/server.py
@@ -0,0 +1,14 @@
+from tornado import web, ioloop
+from sockjs.tornado import SockJSRouter, SockJSConnection
+
+class Echo(SockJSConnection):
+    def on_message(self, msg):
+        print msg
+        self.send(msg)
+
+if __name__ == '__main__':
+    EchoRouter = SockJSRouter(Echo, '/echo', {'disabled_transports':['websocket']})
+
+    app = web.Application(EchoRouter.urls)
+    app.listen(9999)
+    ioloop.IOLoop.instance().start()
